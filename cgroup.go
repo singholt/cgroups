e85ca2e11c6cceaf4a1d7dde2f9d3f8ef51a1d81
Anuj Singh <singholt@amazon.com>
2023-06-08 14:19:43 -0700

    refactor errors for debugging

--- cgroup.go
+++ cgroup.go
@@ -26,8 +26,6 @@ import (
 	"sync"
 
 	v1 "github.com/containerd/cgroups/stats/v1"
-
-	"github.com/opencontainers/runtime-spec/specs-go"
 )
 
 // New returns a new control via the cgroup cgroups interface
@@ -231,7 +229,7 @@ func (c *cgroup) Delete() error {
 		// kernel prevents cgroups with running process from being removed, check the tree is empty
 		procs, err := c.processes(s.Name(), true, cgroupProcs)
 		if err != nil {
-			return err
+			return fmt.Errorf("process check error for %v subsystem, err: %v", string(s.Name()), err)
 		}
 		if len(procs) > 0 {
 			errs = append(errs, fmt.Sprintf("%s (contains running processes)", string(s.Name())))
@@ -240,7 +238,7 @@ func (c *cgroup) Delete() error {
 		if d, ok := s.(deleter); ok {
 			sp, err := c.path(s.Name())
 			if err != nil {
-				return err
+				return fmt.Errorf("deleter error for %v subsystem, err: %v", string(s.Name()), err)
 			}
 			if err := d.Delete(sp); err != nil {
 				errs = append(errs, string(s.Name()))
@@ -250,7 +248,7 @@ func (c *cgroup) Delete() error {
 		if p, ok := s.(pather); ok {
 			sp, err := c.path(s.Name())
 			if err != nil {
-				return err
+				return fmt.Errorf("pather error for %v subsystem, err: %v", string(s.Name()), err)
 			}
 			path := p.Path(sp)
 			if err := remove(path); err != nil {
